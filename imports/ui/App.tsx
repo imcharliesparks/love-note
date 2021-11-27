9c98b08cca1b20040811c368cc09ffed3ab70ed5
Charlie Sparks <sparksfullstack@gmail.com>
2021-11-26 18:35:23 -0800

    Added app bar + material ui

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -9,47 +9,53 @@ import { PrivateRoute } from './components/PrivateRoute'
 import { Home } from './components/Home'
 import { AddPartner } from './components/AddPartner'
 import { PartnerNotes } from './components/PartnerNotes'
+import { MainAppBar } from './components/MainAppBar'
 
-export const App = () => (
-	<ErrorBoundary>
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route
-					path="/my-notes"
-					element={
-						<PrivateRoute>
-							<MyNotes />
-						</PrivateRoute>
-					}
-				/>
-				<Route
-					path="/partner-notes"
-					element={
-						<PrivateRoute>
-							<PartnerNotes />
-						</PrivateRoute>
-					}
-				/>
-				<Route
-					path="/create-note"
-					element={
-						<PrivateRoute>
-							<CreateNote />
-						</PrivateRoute>
-					}
-				/>
-				<Route
-					path="/add-partner"
-					element={
-						<PrivateRoute>
-							<AddPartner />
-						</PrivateRoute>
-					}
-				/>
-				<Route path="/sign-up" element={<SignUp />} />
-				<Route path="/log-in" element={<LogIn />} />
-			</Routes>
-		</Router>
-	</ErrorBoundary>
-)
+export const App = () => {
+	const [isDrawerOpen, setIsDrawerOpen] = React.useState<boolean>(false)
+
+	return (
+		<ErrorBoundary>
+			<Router>
+				<MainAppBar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/my-notes"
+						element={
+							<PrivateRoute>
+								<MyNotes />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/partner-notes"
+						element={
+							<PrivateRoute>
+								<PartnerNotes />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/create-note"
+						element={
+							<PrivateRoute>
+								<CreateNote />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path="/add-partner"
+						element={
+							<PrivateRoute>
+								<AddPartner />
+							</PrivateRoute>
+						}
+					/>
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/log-in" element={<LogIn />} />
+				</Routes>
+			</Router>
+		</ErrorBoundary>
+	)
+}
