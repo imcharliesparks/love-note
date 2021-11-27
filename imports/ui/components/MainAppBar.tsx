96823095877fab4a6847bd74110f1eb4c55856fc
Charlie Sparks <sparksfullstack@gmail.com>
2021-11-27 11:34:38 -0800

    minor tweak made to app bar

--- imports/ui/components/MainAppBar.tsx
+++ imports/ui/components/MainAppBar.tsx
@@ -7,37 +7,46 @@ import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Link } from '@mui/material'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
+import { useLocation, Location } from 'react-router-dom'
 
 type MainAppBarProps = {
 	toggleDrawer: (drawerState: boolean) => void
 	isLoggedIn: boolean
 }
 
-export const MainAppBar = ({ toggleDrawer, isLoggedIn }: MainAppBarProps): React.ReactElement => (
-	<Box sx={{ flexGrow: 1 }}>
-		<AppBar position="static">
-			<Toolbar>
-				<IconButton
-					onClick={() => toggleDrawer(true)}
-					size="large"
-					edge="start"
-					color="inherit"
-					aria-label="menu"
-					sx={{ mr: 2 }}>
-					<MenuIcon />
-				</IconButton>
-				{/* TODO: Convert to react router link */}
-				<Link style={{ color: 'white' }} href="/" variant="h6" component="div" sx={{ flexGrow: 1 }}>
-					LoveNote
-				</Link>
-				{isLoggedIn ? (
-					<AccountCircleIcon />
-				) : (
-					<Button href="/log-in" color="inherit">
-						Login
-					</Button>
-				)}
-			</Toolbar>
-		</AppBar>
-	</Box>
-)
+export const MainAppBar = ({ toggleDrawer, isLoggedIn }: MainAppBarProps): React.ReactElement => {
+	const location: Location = useLocation()
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						onClick={() => toggleDrawer(true)}
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}>
+						<MenuIcon />
+					</IconButton>
+					{/* TODO: Convert to react router link */}
+					<Link
+						style={{ color: 'white' }}
+						href="/"
+						variant="h6"
+						component="div"
+						sx={{ flexGrow: 1 }}>
+						LoveNote
+					</Link>
+					{isLoggedIn ? (
+						<AccountCircleIcon />
+					) : (
+						<Button href="/log-in" color="inherit">
+							{location.pathname.includes('log-in') ? 'Sign Up' : 'Log In'}
+						</Button>
+					)}
+				</Toolbar>
+			</AppBar>
+		</Box>
+	)
+}
